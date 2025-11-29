c4f9db16ea974aee8d2225b92a87891e35d223b7
NoidThes <dion_abeleda@dlsu.edu.ph>
2025-11-29 15:42:30 +0800

    Create InventoryItem.java
    Updated to new one

---
+++ CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/InventoryItem.java
@@ -0,0 +1,27 @@
+package ph.edu.dlsu.lbycpei.caferecommmendationsystem;
+
+
+public class InventoryItem {
+    private String name;
+    private int quantity;
+    private String category;
+
+
+    public InventoryItem(String name, int quantity, String category) {
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+}
